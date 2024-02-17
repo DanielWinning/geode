185fc80c756ea3d544b071cf59a9fa3b55e832b0
Daniel Winning <daniel@winningwebdesign.co.uk>
2024-02-17 15:21:47 +0000

    Remove unimplemented command

--- scripts/snippets/create_sudo_user.sh
+++ scripts/snippets/create_sudo_user.sh
@@ -1,7 +1,7 @@
-echo -e "Creating new user with username $1"
+writeText "Creating new user with username $1"
 adduser --disabled-password --gecos "" --quiet "$1"
 
-echo -e "Granting sudo privileges to $1"
+writeText "Granting sudo privileges to $1"
 usermod -aG sudo "$1"
 usermod -aG www-data "$1"
 rsync --archive --chown="$1":"$1" ~/.ssh /home/"$1"
