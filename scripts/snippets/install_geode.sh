86a2e4cfbbcd87e0c3ace0ca3f77a5d51ae40e12
Daniel Winning <daniel@winningwebdesign.co.uk>
2024-02-17 11:59:45 +0000

    Fix bin path

--- scripts/snippets/install_geode.sh
+++ scripts/snippets/install_geode.sh
@@ -2,7 +2,7 @@ echo -e "Cloning server scripts repository to /usr/local/bin"
 git clone https://github.com/DanielWinning/geode.git /usr/local/bin/geode/
 
 echo -e "Copying geode command to /bin"
-sudo cp /usr/local/bin/geode/geode.sh /bin/geode
+sudo cp /usr/local/bin/geode/bin/geode.sh /bin/geode
 
 echo -e "Making geode command executable"
 sudo chmod -R +x /bin/geode
